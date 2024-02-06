8cdfd9c2cf5b8092492d38b1ecd2c8c626d2e1fd
Rajesh <rajesh.hofo@gmail.com>
2024-02-06 22:47:30 +0800

    fixing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("/app/index")
+  getIndex(): string {
+    return "Hello, I am index.";
+  }
 }
